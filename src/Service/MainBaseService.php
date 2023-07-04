c5911c6acdf2169281ca3a1747f9a1bbc6dbc2c3
Дмитрий Мельнев <dvmau@mail.ru>
2023-07-04 23:47:54 +0300

    main service has done

---
+++ src/Service/MainBaseService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Repository\PaySystemRepository;
+use App\Repository\SectionRepository;
+use App\Repository\SocialRepository;
+
+class MainBaseService
+{
+    private SocialRepository $socialRepository;
+    private PaySystemRepository $paySystemRepository;
+    private SectionRepository $sectionRepository;
+
+    /**
+     * MainBaseService constructor.
+     */
+    public function __construct(
+        SocialRepository $socialRepository,
+        PaySystemRepository $paySystemRepository,
+        SectionRepository $sectionRepository
+    )
+    {
+
+        $this->socialRepository = $socialRepository;
+        $this->paySystemRepository = $paySystemRepository;
+        $this->sectionRepository = $sectionRepository;
+    }
+
+    public function getSocial(): ?array
+    {
+        return $this->socialRepository->findAll();
+    }
+
+    public function getPaySystem(): ?array
+    {
+        return $this->paySystemRepository->findAll();
+    }
+
+    public function getCategories(): ?array
+    {
+        $this->sectionRepository->getArray();
+    }
+
+    public function getAllBase(): ?array
+    {
+        $result = [];
+        $result['social'] = $this->getSocial();
+        $result['paySystems'] = $this->getPaySystem();
+        $result['categories'] = $this->getCategories();
+        return $result;
+    }
+
+}
