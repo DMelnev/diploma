b699c9ae90b06697f9a603f5c0a1b66ab5368217
Дмитрий Мельнев <dvmau@mail.ru>
2023-07-04 23:59:19 +0300

    main service has done

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,88 +2,44 @@
 
 namespace App\Controller;
 
-use App\Repository\BannerRepository;
-use App\Repository\CartProductRepository;
-use App\Repository\DayOfferRepository;
-use App\Repository\PaySystemRepository;
-use App\Repository\ProductRepository;
-use App\Repository\SectionRepository;
-use App\Repository\SocialRepository;
+use App\Service\MainService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    private SocialRepository $socialRepository;
-    private SectionRepository $sectionRepository;
-    private BannerRepository $bannerRepository;
-    private CartProductRepository $cartProductRepository;
-    private DayOfferRepository $dayOfferRepository;
-    private ProductRepository $productRepository;
-    private PaySystemRepository $paySystemRepository;
+    private MainService $service;
 
     /**
      * MainController constructor.
      */
-    public function __construct(
-        SocialRepository $socialRepository,
-        SectionRepository $sectionRepository,
-        BannerRepository $bannerRepository,
-        CartProductRepository $cartProductRepository,
-        DayOfferRepository $dayOfferRepository,
-        ProductRepository $productRepository,
-        PaySystemRepository $paySystemRepository)
+    public function __construct(MainService $service)
     {
-        $this->socialRepository = $socialRepository;
-        $this->sectionRepository = $sectionRepository;
-        $this->bannerRepository = $bannerRepository;
-        $this->cartProductRepository = $cartProductRepository;
-        $this->dayOfferRepository = $dayOfferRepository;
-        $this->productRepository = $productRepository;
-        $this->paySystemRepository = $paySystemRepository;
+        $this->service = $service;
     }
 
     /**
      * @Route("/", name="app_main")
      */
     public function index(): Response
-
     {
-        $dayOffer = $this->dayOfferRepository->getOffer();
-        return $this->render('main/index.html.twig', [
-            'paySystems' => $this->paySystemRepository->findAll(),
-            'social' => $this->socialRepository->findAll(),
-            'categories' => $this->sectionRepository->getArray(),
-            'banners' => $this->bannerRepository->getRandom($this->getParameter('main.count_of_banners')),
-            'products' => $this->cartProductRepository->getTopProducts(),
-            'dayOffer' => $dayOffer,
-            'hotOffer' => $this->productRepository->getRandomAction($this->getParameter('main.count_of_actions')),
-            'limited' => $this->productRepository->getLimited($this->getParameter('main.count_of_limited'), $dayOffer['id'])
-        ]);
+        return $this->render('main/index.html.twig', $this->service->getAll());
     }
 
     /**
      * @Route("/about", name="app_about")
      */
-    public function about():Response
+    public function about(): Response
     {
-        return $this->render('main/about.html.twig',[
-            'paySystems' => $this->paySystemRepository->findAll(),
-            'social' => $this->socialRepository->findAll(),
-            'categories' => $this->sectionRepository->getArray(),
-        ]);
+        return $this->render('main/about.html.twig', $this->service->getAllBase());
     }
 
     /**
      * @Route("/contacts", name="app_contacts")
      */
-    public function contacts():Response
+    public function contacts(): Response
     {
-        return $this->render('main/contacts.html.twig',[
-            'paySystems' => $this->paySystemRepository->findAll(),
-            'social' => $this->socialRepository->findAll(),
-            'categories' => $this->sectionRepository->getArray(),
-        ]);
+        return $this->render('main/contacts.html.twig', $this->service->getAllBase());
     }
 }
